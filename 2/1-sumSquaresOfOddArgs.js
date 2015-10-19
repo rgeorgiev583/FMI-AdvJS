579193e5a4630bf53a743c0913bde0dd00f9c9a3
Radoslav Georgiev <rgeorgiev583@gmail.com>
2015-10-19 19:18:46 +0300

    style fix: reformatted code according to JS conventions

--- 2/1-sumSquaresOfOddArgs.js
+++ 2/1-sumSquaresOfOddArgs.js
@@ -1,12 +1,10 @@
 function sumSquaresOfOddArgs()
 {
     return [].slice.call(arguments)
-            .filter(function (value, index)
-            {
+            .filter(function (value, index) {
                 return index % 2 == 0;
             })
-            .reduce(function (total, value)
-            {
+            .reduce(function (total, value) {
                 return total + value * value;
             }, 0);
 }
